93a577a562e3a6c8dc4d9b6c6db1fa505a9ada6f
Lauri <lauri.nomme@gmail.com>
2017-10-25 11:17:02 +0300

    Optimize output

---
+++ Makefile
@@ -0,0 +1,5 @@
+sort10M: sort10M.c
+	gcc -std=gnu11 -lpthread -O3 -o sort10M sort10M.c
+
+test: sort10M
+	rm -f out.txt && time ./sort10M numbers.txt out.txt && echo '41398970ebd7cb3aa8c38c35bb5b4339  out.txt' | md5sum -c -
--- sort10M.c
+++ sort10M.c
@@ -7,34 +7,29 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <pthread.h>
 
+#define ELEMENT_MAX 9999999
+#define ELEMENT_SIZE 8
+
 int workerCount = 4;
-char* presentElements;
+char** presentElements;
 char* memIn;
 int elementCount;
-int elementSize = 8;
 
+int mapInput(char* fileName, char** memIn, int* elementCount);
 void* processPartition(void* workerIndex);
-void saveResult(char* fileName);
+int saveResult(char* fileName, int elementCount);
 
 int main(int argc, char** argv) {
-    int fdIn;
-    struct stat stat;
     pthread_t workers[workerCount];
 
-    presentElements = calloc(10 * 1000 * 1000, 1);
+    presentElements = (char**)calloc(ELEMENT_MAX + 1, sizeof(char*));
 
-    fdIn = open(argv[1], O_RDONLY);
-    fstat(fdIn, &stat);
-    elementCount = stat.st_size / elementSize;
-
-    if (MAP_FAILED == (memIn = mmap(NULL, stat.st_size, PROT_READ, MAP_SHARED | MAP_POPULATE, fdIn, 0))) {
-        perror("mmap infile");
+    if (0 != mapInput(argv[1], &memIn, &elementCount)) {
         return 1;
     }
-
-    madvise(memIn, stat.st_size, POSIX_MADV_SEQUENTIAL);
 
     for (long i = 0; i < workerCount; ++i) {
         pthread_create(&workers[i], NULL, &processPartition, (void*)i);
@@ -44,25 +39,77 @@ int main(int argc, char** argv) {
         pthread_join(workers[i], NULL);
     }
 
-    saveResult(argv[2]);
+    if (0 != saveResult(argv[2], elementCount)) {
+        return 1;
+    }
 
+    return 0;
+}
+
+int mapInput(char* fileName, char** memIn, int* elementCount) {
+    int fdIn;
+    struct stat stat;
+
+    if (-1 == (fdIn = open(fileName, O_RDONLY))) {
+        perror("open infile");
+        return -1;
+    }
+
+    fstat(fdIn, &stat);
+    *elementCount = stat.st_size / ELEMENT_SIZE;
+
+    if (MAP_FAILED == (*memIn = (char*)mmap(NULL, stat.st_size, PROT_READ, MAP_SHARED | MAP_POPULATE, fdIn, 0))) {
+        perror("mmap infile");
+        return -1;
+    }
+
+    madvise(*memIn, stat.st_size, MADV_SEQUENTIAL);
     return 0;
 }
 
 void* processPartition(void* workerIndex) {
     for (long i = (long)workerIndex; i < elementCount; i += workerCount) {
-        int number = strtol(memIn + elementSize * i, NULL, 10);
-        presentElements[number] = 1;
+        int number = strtol(memIn + ELEMENT_SIZE * i, NULL, 10);
+        presentElements[number] = memIn + ELEMENT_SIZE * i;
     }
 }
 
-void saveResult(char* fileName) {
-    FILE* fOut = fopen(fileName, "w");
-    for (int i = 0; i <= 9999999; ++i) {
+int saveResult(char* fileName, int elementCount) {
+    int fdOut;
+    char* memOut;
+
+    if (-1 == (fdOut = open(fileName, O_RDWR | O_CREAT | O_TRUNC, 0755))) {
+        perror("open outfile");
+        return -1;
+    }
+
+    if (-1 == lseek(fdOut, elementCount * ELEMENT_SIZE - 1, SEEK_SET)) {
+        perror("seek outfile");
+        return -1;
+    }
+
+    if (-1 == write(fdOut, "", 1)) {
+        perror("write outfile");
+        return -1;
+    }
+
+    if (MAP_FAILED == (memOut = (char*)mmap(NULL, elementCount * ELEMENT_SIZE, PROT_WRITE, MAP_SHARED | MAP_POPULATE, fdOut, 0))) {
+        perror("mmap outfile");
+        return -1;
+    }
+
+    madvise(memOut, elementCount * ELEMENT_SIZE, MADV_SEQUENTIAL);
+
+    char* outPtr = memOut;
+    for (int i = 0; i <= ELEMENT_MAX; ++i) {
         if (presentElements[i]) {
-            fprintf(fOut, "%07i\n", i);
+            *(uint64_t*)outPtr = *(uint64_t*)presentElements[i];
+            outPtr += ELEMENT_SIZE;
         }
     }
 
-    fclose(fOut);
+    munmap(memOut, elementCount * ELEMENT_SIZE);
+    close(fdOut);
+
+    return 0;
 }
