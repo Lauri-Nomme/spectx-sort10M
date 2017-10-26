b247f56e715b1f1246db99eaeee2eaa248e9101c
Lauri <lauri.nomme@gmail.com>
2017-10-26 17:54:40 +0300

    Lock threads to distinct CPUs Autodetect CPU count, use as number of workers Instead of building an array of pointers, build an array of element content in partition processors, in output phase, scan for edges of continuous block in array and copy the block as a whole to output file Replace atoi with ascii -> bcd + multiply-add loop For stretching output file, use truncate instead of seek + write

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 sort10M: sort10M.c
-	gcc -std=gnu11 -pthread -O3 -o sort10M sort10M.c
+	gcc -std=gnu11 -pthread -O3 -march=native -mfpmath=sse -o sort10M sort10M.c
 
 test: sort10M
 	rm -f out.txt && time ./sort10M numbers.txt out.txt && echo '41398970ebd7cb3aa8c38c35bb5b4339  out.txt' | md5sum -c -
--- sort10M.c
+++ sort10M.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
@@ -9,6 +11,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sched.h>
 
 #define ELEMENT_MAX 9999999
 #define ELEMENT_SIZE 8
@@ -17,19 +20,24 @@
 #define MAP_POPULATE 0
 #endif
 
-int workerCount = 4;
-char** presentElements;
+int workerCount;
+uint64_t* presentElements;
 char* memIn;
 int elementCount;
 
 int mapInput(char* fileName, char** memIn, int* elementCount);
 void* processPartition(void* workerIndex);
 int saveResult(char* fileName, int elementCount);
+int getProcessorCount();
+int setSelfAffinitySingleCPU(int cpu);
 
 int main(int argc, char** argv) {
+    workerCount = getProcessorCount();
+    printf("workerCount - %d\n", workerCount);
+    
     pthread_t workers[workerCount];
 
-    presentElements = (char**)calloc(ELEMENT_MAX + 1, sizeof(char*));
+    presentElements = (uint64_t*)calloc(ELEMENT_MAX + 1, sizeof(char*));
 
     if (0 != mapInput(argv[1], &memIn, &elementCount)) {
         return 1;
@@ -50,7 +58,7 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-int mapInput(char* fileName, char** memIn, int* elementCount) {
+inline int mapInput(char* fileName, char** memIn, int* elementCount) {
     int fdIn;
     struct stat stat;
 
@@ -71,16 +79,53 @@ int mapInput(char* fileName, char** memIn, int* elementCount) {
     return 0;
 }
 
-void* processPartition(void* workerIndex) {
-    for (long i = (long)workerIndex; i < elementCount; i += workerCount) {
-        int number = strtol(memIn + ELEMENT_SIZE * i, NULL, 10);
-        presentElements[number] = memIn + ELEMENT_SIZE * i;
+inline unsigned int strtolb10(char* str) {
+    uint64_t tmp = *(uint64_t*)str - 0x0030303030303030;
+    char* tmpStr = (char*)&tmp;
+    //printf("%02X %02X %02X %02X %02X %02X %02X %02X \n", tmpStr[0], tmpStr[1], tmpStr[2], tmpStr[3], tmpStr[4], tmpStr[5], tmpStr[6], tmpStr[7]);
+    unsigned int res = 0;
+    /*res += tmp & 0xFF;
+    res *= 10;
+    tmp >>= 8;
+    res += tmp & 0xFF;
+    res *= 10;
+    tmp >>= 8;
+    res += tmp & 0xFF;
+    res *= 10;
+    tmp >>= 8;
+    res += tmp & 0xFF;
+    res *= 10;
+    tmp >>= 8;
+    res += tmp & 0xFF;
+    res *= 10;
+    tmp >>= 8;
+    res += tmp & 0xFF;
+    res *= 10;
+    tmp >>= 8;
+    res += tmp & 0xFF;*/
+    for (unsigned int i = 0; i < 7; ++i) {
+        res = res * 10 + *tmpStr++;
+    }
+    return res;
+    //return (tmpStr[6] + 10 * (tmpStr[5] + 10 * (tmpStr[4] + 10 * (tmpStr[3] + 10 * (tmpStr[2] + 10 * (tmpStr[1] + 10 * tmpStr[0]))))));
+    //return atoi(str);
+}
+
+void* processPartition(void* arg) {
+    long workerIndex = (long)arg;
+
+	setSelfAffinitySingleCPU(workerIndex);
+
+    for (long i = workerIndex; i < elementCount; i += workerCount) {
+        char* s = memIn + ELEMENT_SIZE * i;
+        unsigned int element = strtolb10(s);
+        presentElements[element] = *(uint64_t*)s;
     }
 
     return 0;
 }
 
-int saveResult(char* fileName, int elementCount) {
+inline int saveResult(char* fileName, int elementCount) {
     int fdOut;
     char* memOut;
 
@@ -89,17 +134,13 @@ int saveResult(char* fileName, int elementCount) {
         return -1;
     }
 
-    if (-1 == lseek(fdOut, elementCount * ELEMENT_SIZE - 1, SEEK_SET)) {
-        perror("seek outfile");
+    if (-1 == ftruncate(fdOut, elementCount * ELEMENT_SIZE)) {
+        perror("ftruncate outfile");
         return -1;
     }
 
-    if (-1 == write(fdOut, "", 1)) {
-        perror("write outfile");
-        return -1;
-    }
-
-    if (MAP_FAILED == (memOut = (char*)mmap(NULL, elementCount * ELEMENT_SIZE, PROT_WRITE, MAP_SHARED | MAP_POPULATE, fdOut, 0))) {
+    if (MAP_FAILED ==
+        (memOut = (char*)mmap(NULL, elementCount * ELEMENT_SIZE, PROT_WRITE, MAP_SHARED | MAP_POPULATE, fdOut, 0))) {
         perror("mmap outfile");
         return -1;
     }
@@ -107,15 +148,50 @@ int saveResult(char* fileName, int elementCount) {
     madvise(memOut, elementCount * ELEMENT_SIZE, MADV_SEQUENTIAL);
 
     char* outPtr = memOut;
-    for (int i = 0; i <= ELEMENT_MAX; ++i) {
+	uint64_t* rangeBegin = presentElements;
+	uint64_t* rangeEnd; 
+	do {
+		// first present element
+		while (!*rangeBegin) {
+			++rangeBegin;
+		}
+
+		 // first missing element
+		rangeEnd = rangeBegin;
+		while (*rangeEnd) {
+			++rangeEnd;
+		}
+
+		memcpy(outPtr, rangeBegin, (rangeEnd - rangeBegin) * ELEMENT_SIZE);
+		outPtr += (rangeEnd - rangeBegin) * ELEMENT_SIZE;
+		rangeBegin = rangeEnd;
+	} while (rangeBegin < &presentElements[ELEMENT_MAX] && rangeEnd < &presentElements[ELEMENT_MAX]);
+
+    /*for (int i = 0; i <= ELEMENT_MAX; ++i) {
         if (presentElements[i]) {
-            *(uint64_t*)outPtr = *(uint64_t*)presentElements[i];
+            *(uint64_t *) outPtr = *(uint64_t * )presentElements[i];
             outPtr += ELEMENT_SIZE;
         }
-    }
+    }*/
 
     munmap(memOut, elementCount * ELEMENT_SIZE);
     close(fdOut);
 
     return 0;
+}
+
+int getProcessorCount() {
+	return sysconf(_SC_NPROCESSORS_ONLN);
+}
+
+int setSelfAffinitySingleCPU(int cpu) {
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(cpu, &cpuset);
+    if (-1 == sched_setaffinity(0, sizeof(cpu_set_t), &cpuset)) {
+        perror("sched_setaffinity");
+		return -1;
+    }
+
+	return 0;
 }
